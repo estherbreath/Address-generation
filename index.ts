5c35a173ce98bedbdcc9a2f159e7d789d3f5b10a
Esther Breath <estherojile248@gmail.com>
2023-08-05 17:43:41 +0000

    first commit

---
+++ index.ts
@@ -0,0 +1,5 @@
+import  * as ethers from 'ethers';
+const mnemonic: string = 'injury manage trade drama violin wrist rack eyebrow patient fiscal stuff clerk';
+
+const wallet = ethers.Wallet.fromPhrase(mnemonic);
+console.log(wallet);
